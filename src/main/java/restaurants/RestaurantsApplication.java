d6178b088b30c1eeab77f2ae9ce4c61642fa2dc7
matt on laptop <matt@matthew>
2020-11-13 15:28:55 +0000

    First commit

---
+++ src/main/java/restaurants/RestaurantsApplication.java
@@ -0,0 +1,11 @@
+package restaurants;
+
+import org.springframework.boot.SpringApplication;
+
+public class RestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/restaurants/controller/RestaurantController.java
@@ -0,0 +1,10 @@
+package restaurants.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("restaurants")
+public class RestaurantController {
+
+}
