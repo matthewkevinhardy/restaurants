8518bd66792067b22bba3935c070f58697bd4dec
matt on laptop <matt@matthew>
2020-11-19 14:36:29 +0000

    Tidy

--- src/test/java/restaurants/controller/TableControllerTest.java
+++ src/test/java/restaurants/controller/TableControllerTest.java
@@ -2,10 +2,7 @@ package restaurants.controller;
 
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import restaurants.model.RestaurantTable;
 
 @SpringBootTest
 @AutoConfigureMockMvc
